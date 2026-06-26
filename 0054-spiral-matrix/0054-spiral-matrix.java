0a9ac2794e85b29755e6c58d6e205e95cca9f5e4
Aaditya Mathur <104914201+adityamathur456@users.noreply.github.com>
2026-06-26 19:02:04 +0530

    Time: 0 ms (100%), Space: 43 MB (60.61%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int[] steps = {matrix[0].length, matrix.length - 1};
+
+        int r = 0, c = -1, d = 0;
+        while (steps[d % 2] > 0) {
+            for (int i = 0; i < steps[d % 2]; i++) {
+                r += dir[d][0];
+                c += dir[d][1];
+                res.add(matrix[r][c]);
+            }
+
+            steps[d % 2]--;
+            d = (d + 1) % 4;
+        }
+
+        return res;
+    }
+}
