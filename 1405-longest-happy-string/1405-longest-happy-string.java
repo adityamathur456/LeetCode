5f3e7f21cfd7ce294911ab1b8dcac0de05ad2311
Aaditya Mathur <adityamathur989@gmail.com>
2026-08-13 18:30:23 +0530

    Time: 3 ms (19.35%), Space: 42.8 MB (18.62%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String longestDiverseString(int a, int b, int c) {
+        StringBuilder res = new StringBuilder();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> y[0] - x[0]);
+
+        if (a > 0) pq.offer(new int[]{a, 'a'});
+        if (b > 0) pq.offer(new int[]{b, 'b'});
+        if (c > 0) pq.offer(new int[]{c, 'c'});
+
+        while (!pq.isEmpty()) {
+            int[] first = pq.poll();
+            if (res.length() > 1 && res.charAt(res.length() - 1) == first[1] && res.charAt(res.length() - 2) == first[1]) {
+                if (pq.isEmpty()) break;
+                int[] second = pq.poll();
+                res.append((char) second[1]);
+                second[0]--;
+                if (second[0] > 0)
+                    pq.offer(second);
+                pq.offer(first);
+            } else {
+                res.append((char) first[1]);
+                first[0]--;
+                if (first[0] > 0)
+                    pq.offer(first);
+            }
+        }
+
+        return res.toString();
+    }
+}
